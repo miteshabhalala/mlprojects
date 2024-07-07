34f11cf2ca18d426c924ed21b1dbcb7285f7dd0e
miteshabhalala <mitesha2107@gmail.com>
2024-07-07 11:55:34 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
+
+HYPEN_E_DOT ='-e .'
 def get_requirements(file_path:str)-> List[str]:
     '''
     this function will return the list pf requirements
@@ -10,11 +12,15 @@ def get_requirements(file_path:str)-> List[str]:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","")for req in requirements]
 
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
 setup(
 name = 'mlproject'
 version ='0.0.1',
-author=mitesha,
+author=Mitesha,
 author_email='mitesha2107@gmail.com'
 packages=find_packages(),
 install_requires=get_requirements('requirement.txt')
-)
+)   
